d29ddaef67b3a41cbf91f2c238ec5c749923a9ae
afonsoaoc <afonso.aoc@gmail.com>
2018-08-17 22:38:37 -0300

    adicionado instalacao automatica do puppet

---
+++ musicjungle/manifests/puppet.sh
@@ -0,0 +1,14 @@
+echo "Adding puppet repo"
+sudo apt-get update
+echo "installing puppet"
+sudo apt-get -y install puppetmaster
+sudo apt-get -y install puppet
+
+echo "ensure puppet service is running"
+sudo puppet resource service puppet ensure=running enable=true
+#echo "ensure puppet service is running"
+#sudo puppet resource service puppetmaster ensure=running enable=true
+
+echo "ensure puppet service is running for standalone install"
+sudo puppet resource cron puppet-apply ensure=present user=root minute=30 command='/usr/bin/puppet apply $(puppet apply --configprint manifest)'
+
